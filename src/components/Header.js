13f80fa5d07f648eb45862085c6c586f51278539
Felipe Rangel <feliperangel277@gmail.com>
2022-03-08 18:38:32 -0300

    Adiciona requisito 6 - implementa funcao de pesquisar

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,7 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Carregando from '../pages/Carregando';
 import { getUser } from '../services/userAPI';
-import { Link } from 'react-router-dom';
 
 class Header extends React.Component {
   constructor() {
--- src/pages/Search.js
+++ src/pages/Search.js
@@ -1,5 +1,8 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import searchAlbumsAPI from '../services/searchAlbumsAPI';
 import Header from '../components/Header';
+import Carregando from './Carregando';
 
 class Search extends React.Component {
   constructor() {
@@ -8,12 +11,28 @@ class Search extends React.Component {
     this.state = {
       buttonIsDisabled: true,
       search: '',
+      searchFinal: '',
+      isLoading: false,
+      listOfMusics: [],
     };
     this.validate = this.validate.bind(this);
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  async handleClick(event) {
+    event.preventDefault();
+    const { search } = this.state;
+    const searchToFind = search;
+    this.setState({
+      search: '',
+      searchFinal: searchToFind,
+      isLoading: true,
+    });
+    const listOfItems = await searchAlbumsAPI(searchToFind);
+    this.setState({ isLoading: false, listOfMusics: listOfItems });
   }
 
   validate(event) {
-    event.preventDefault();
     const newSearchValue = event.target.value;
     const minLengthSearch = 2;
     if (newSearchValue.length >= minLengthSearch) {
@@ -24,30 +43,52 @@ class Search extends React.Component {
   }
 
   render() {
-    const { buttonIsDisabled, search } = this.state;
+    const { buttonIsDisabled, search, searchFinal, isLoading, listOfMusics } = this.state;
     return (
       <div data-testid="page-search">
         <Header />
-        <form>
-          <label htmlFor="search">
-            Nome:
-            <input
-              type="text"
-              id="search"
-              value={ search }
-              data-testid="search-artist-input"
-              onChange={ this.validate }
-            />
-          </label>
-          <button
-            type="submit"
-            data-testid="search-artist-button"
-            disabled={ buttonIsDisabled }
-            // onClick={ this.handleClick }
-          >
-            Pesquisar
-          </button>
-        </form>
+        {isLoading
+          ? <Carregando />
+          : (
+            <form>
+              <label htmlFor="search">
+                Nome:
+                <input
+                  type="text"
+                  id="search"
+                  value={ search }
+                  data-testid="search-artist-input"
+                  onChange={ this.validate }
+                />
+              </label>
+              <button
+                type="submit"
+                data-testid="search-artist-button"
+                disabled={ buttonIsDisabled }
+                onClick={ this.handleClick }
+              >
+                Pesquisar
+              </button>
+            </form>
+          )}
+        { searchFinal.length > 0 && `Resultado de álbuns de: ${searchFinal}`}
+        {(listOfMusics.length > 0)
+          ? (listOfMusics.map((element) => (
+            <div key={ element.collectionId }>
+              <Link
+                to={ `/album/${element.collectionId}` }
+                data-testid={ `link-to-album-${element.collectionId}` }
+              >
+                <img
+                  src={ element.artworkUrl100 }
+                  alt={ `Capa do album: ${element.collectionName}` }
+                />
+              </Link>
+              <p>{ element.collectionName }</p>
+              <p>{ element.artistName }</p>
+            </div>
+          )))
+          : <p>Nenhum álbum foi encontrado</p>}
       </div>
     );
   }
